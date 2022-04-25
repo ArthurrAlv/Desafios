65b61f532ac7fac42dc86d9efa58b542cd4306bd
Lucas Câmara <lfcamara.ti@gmail.com>
2022-04-25 13:38:57 -0300

    Desafio CalculoIdade

---
+++ CalculoIdade.java
@@ -0,0 +1,7 @@
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		// Crie um código que lê o ano de nascimento e calcule a idade que a pessoa tem
+		// Para recuperar a data atual vc precisará usar a classe LocalDateTime (na dúvida de como usar essa classe, dê uma pesquisada sobre ela na internet)
+	}
+}

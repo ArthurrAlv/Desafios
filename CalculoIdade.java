3a04fb4972dd0da110364ed727edb83da7bce6e5
ArthurrAlv <104282615+ArthurrAlv@users.noreply.github.com>
2022-05-12 11:06:28 -0300

    Update CalculoIdade.java

--- CalculoIdade.java
+++ CalculoIdade.java
@@ -1,7 +1,43 @@
-public class Main {
+publiimport java.util.Scanner;
+import java.time.LocalDate;
+class Main { 
+  public static void main(String args[]) { 
+	int dia, mes, ano, id, mesnow, dianow;
+	String nome;
+	  System.out.println("Olá qual o seu nome:");
+    Scanner ler = new Scanner(System.in);
+	    nome = ler.nextLine();
+    System.out.println(nome+", agora digite a sua data de nascimento:");
+
+    System.out.printf("\n");
+    
+    System.out.printf("Dia: ");
+      dia = ler.nextInt();
+    
+    System.out.printf("\
+    
+    System.out.printf("Mês: ");
+      mes = ler.nextInt();
+    
+    System.out.printf("\n");
+    
+    System.out.printf("Ano: ");
+      ano = ler.nextInt();
+    
+    System.out.printf("\n");
+    
+    LocalDate localDate = LocalDate.now();
+    id = localDate.getYear();
+    mesnow =  localDate.getMonthValue();
+    dianow =  localDate.getDayOfWeek().ordinal();
+    id= id - ano;
+  if(mesnow<mes && dianow<dia)
+    id--;
+    else{
+      
+    }
+    System.out.println("Você tem "+id+" anos.");
 	
-	public static void main(String[] args) throws Exception {
-		// Crie um código que lê o ano de nascimento e calcule a idade que a pessoa tem
-		// Para recuperar a data atual vc precisará usar a classe LocalDateTime (na dúvida de como usar essa classe, dê uma pesquisada sobre ela na internet)
-	}
+
 }
+}	
